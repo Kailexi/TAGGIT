93a902e106467f134c532b6dc34f58f97205cfa0
Kailexi <72436264+Kailexi@users.noreply.github.com>
2025-10-18 12:28:17 +0300

    Resolve Jump Bug, Stamina Updates, Code Refactor, Debug Tools Added

--- Source/ProjectTaggit/InputPlayer/InputCharacter.cpp
+++ Source/ProjectTaggit/InputPlayer/InputCharacter.cpp
@@ -46,46 +46,45 @@ void AInputCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 		// Bind the input actions
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AInputCharacter::Move);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AInputCharacter::Look);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &AInputCharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AInputCharacter::Jump);
 		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Triggered, this, &AInputCharacter::StartSprint);
 		EnhancedInputComponent->BindAction(SprintAction, ETriggerEvent::Completed , this, &AInputCharacter::EndSprint);
 	}
 }
 
-void AInputCharacter::Move(const FInputActionValue& InputValue)
+float AInputCharacter::GetStaminaForHUD() const
 {
-	FVector2D InputVector = InputValue.Get<FVector2D>();
-	if (IsValid(Controller))
-	{
-		//Get forward/backward direction
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
-	
-		// Add movement input
-		AddMovementInput(ForwardDirection, InputVector.Y);
-		AddMovementInput(RightDirection, InputVector.X);
-	}
+	return StaminaComponent ? StaminaComponent->GetCurrentStamina() : 0.0f;
 }
 
-void AInputCharacter::Look(const FInputActionValue& InputValue)
+float AInputCharacter::GetMaxStaminaForHUD() const
 {
-	FVector2D InputVector = InputValue.Get<FVector2D>();
-	if (IsValid(Controller))
-	{
-		AddControllerYawInput(InputVector.X);
-		AddControllerPitchInput(InputVector.Y);
-	}
+	return StaminaComponent ? StaminaComponent->GetMaxStamina() : 0.0f;
 }
+
+bool bIsJumping = false;
 
 void AInputCharacter::Jump()
 {
-	if (StaminaComponent->TryConsumeStamina(JumpStaminaCost)) 
-	{
-		ACharacter::Jump();
-	}
+    if (bIsJumping)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Jump already in progress!"));
+        return;
+    }
+
+    bIsJumping = true;
+    UE_LOG(LogTemp, Warning, TEXT("CurrentStamina before jump: %f"), StaminaComponent->GetCurrentStamina());
+    if (StaminaComponent->TryConsumeStamina(JumpStaminaCost)) 
+    {
+        ACharacter::Jump();
+    }
+    UE_LOG(LogTemp, Warning, TEXT("CurrentStamina after jump: %f"), StaminaComponent->GetCurrentStamina());
+}
+
+void AInputCharacter::Landed(const FHitResult& Hit)
+{
+    Super::Landed(Hit);
+    bIsJumping = false; // Reset the flag when the character lands
 }
 
 void AInputCharacter::StartSprint()
@@ -103,11 +102,30 @@ void AInputCharacter::EndSprint()
 	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
 }
 
-float AInputCharacter::GetStaminaForHUD() const
+void AInputCharacter::Move(const FInputActionValue& InputValue)
 {
-    return StaminaComponent ? StaminaComponent->GetCurrentStamina() : 0.0f;
+	FVector2D InputVector = InputValue.Get<FVector2D>();
+	if (IsValid(Controller))
+	{
+		//Get forward/backward direction
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// Add movement input
+		AddMovementInput(ForwardDirection, InputVector.Y);
+		AddMovementInput(RightDirection, InputVector.X);
+	}
 }
 
-
-
-
+void AInputCharacter::Look(const FInputActionValue& InputValue)
+{
+	FVector2D InputVector = InputValue.Get<FVector2D>();
+	if (IsValid(Controller))
+	{
+		AddControllerYawInput(InputVector.X);
+		AddControllerPitchInput(InputVector.Y);
+	}
+}
