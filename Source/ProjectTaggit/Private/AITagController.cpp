46231a571548e5e7b76b261a9ff959f2b759510c
Kailexi <72436264+Kailexi@users.noreply.github.com>
2025-12-19 19:33:13 +0300

    Change comment lul

--- Source/ProjectTaggit/Private/AITagController.cpp
+++ Source/ProjectTaggit/Private/AITagController.cpp
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "AITagController.h"
 #include "AITagCharacter.h"
 #include "ProjectTaggit/InputPlayer/InputCharacter.h"
